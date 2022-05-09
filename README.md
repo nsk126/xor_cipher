f1dcfc6bc05e6368d35d0dcd7daebdcc885a92f5
Suresh <nsk126@gmail.com>
2022-05-09 22:20:59 +0530

    prob final commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# A XOR cipher
+
+Wanted to see some simple forms of encryption and found the xor-cipher. Here is a casual attempt at it.
+
+
+## Build guide
+```
+git clone <this repo>
+mkdir build
+cd build
+cmake ..
+make
+./xorCipher <your input string>
+```
+
+## Notes
+
+- Made to only work with one argument.
+- all inputs are treated as ASCII chars
+
+### Open an issue if you see any improvements.
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ void cipher(int key, char* txt)
 {
     size_t len = strlen(txt);
 
-    // Your cipher input
+    // Simple XOR Cipher
     for (int i = 0; i < len; i++)
     {
         txt[i] = (char) (key ^ (int) txt[i]);
@@ -18,13 +18,14 @@ int main(int a, char** v) {
 
     int randomCipher = 64014; // Add your custom cipher here
 
+    // Consider only one argument
     char* toC = v[a - 1];
 
-    std::cout << toC << std::endl;    
+    std::cout << toC << std::endl; // Will print your input string
     cipher(randomCipher, toC);
-    std::cout << toC << std::endl;
+    std::cout << toC << std::endl; // Will print the encrypted string
     cipher(randomCipher, toC);
-    std::cout << toC << std::endl;
+    std::cout << toC << std::endl; // Will print the decrypted string
     
     
 
